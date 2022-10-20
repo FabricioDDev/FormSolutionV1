17d90319e1366cbec7ed91bd4c7f4b0543b4daa0
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2022-10-20 17:46:28 +0200

    UserDataUpdate

--- DataModel/SexData.cs
+++ DataModel/SexData.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
+using DB;
 
 namespace DataModel
 {
-    class SexData
+    public class SexData
     {
+        public SexData()
+        {
+            data = new DataAccess();
+        }
+        private DataAccess data;
+        public List<Sex> Listing()
+        {
+            List<Sex> List = new List<Sex>();
+            try
+            {
+                data.Query("select Id, Sex from SexTable");
+                data.Read();
+                while (data.PropReader.Read())
+                {
+                    Sex aux = new Sex();
+                    aux.Id = (int)data.PropReader["Id"];
+                    aux.Description = (string)data.PropReader["Sex"];
+                }
+                return List;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally { data.Close(); }
+        }
     }
 }
--- DataModel/UserData.cs
+++ DataModel/UserData.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
+using DB;
 
 namespace DataModel
 {
     public class UserData
     {
+        public UserData()
+        {
+            data = new DataAccess();
+        }
+        private DataAccess data;
+
+        public List<User>Listing()
+        {
+            List<User> List = new List<User>();
+            try
+            {
+                data.Sp("ProcedureUserList");
+                data.Read();
+                while (data.PropReader.Read())
+                {
+                    User aux = new User();
+                    aux.Id = (int)data.PropReader["IdUser"];
+                    aux.UserName = (string)data.PropReader["UserName"];
+                    aux.UserSubName = (string)data.PropReader["UserSubName"];
+                    aux.UserMail = (string)data.PropReader["UserMail"];
+                    aux.UserPass = (string)data.PropReader["UserPass"];
+                    aux.BornDate = (DateTime)data.PropReader["BornDate"];
+                    aux.Country = (string)data.PropReader["Country"];
+                    aux.sex = new Sex();
+                    aux.sex.Id = (int)data.PropReader["Id"];
+                    aux.sex.Description = (string)data.PropReader["Sex"];
+                }
+                return List;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally { data.Close(); }
+        }
     }
 }
